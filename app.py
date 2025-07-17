077da1fd9e6f36933b220e7991d501dc67f9ed33
Ahmedjamal311 <ahmedjamal.jsr@gmail.com>
2025-07-17 15:15:45 -0400

    Base completed and fucntioning. extra stuff will be added later

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from PyQt6.QtWidgets import QWidget, QLabel, QPushButton, QLineEdit, QComboBox, QDateEdit, QTableWidget, QVBoxLayout, QHBoxLayout, QMessageBox, QTableWidgetItem, QHeaderView
 
 from PyQt6.QtCore import QDate, Qt
+from database import fetch_expenses, add_expenses, delete_expenses
 
 class ExpenseApp(QWidget):
     def __init__(self):
         super().__init__()
         self.settings()
         self.initUI()
+        self.load_table_data()
     
     def settings(self):
         self.setGeometry(750, 300, 550, 500)
@@ -27,6 +29,9 @@ class ExpenseApp(QWidget):
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
 
         self.populate_dropdown()
+
+        self.btn_add.clicked.connect(self.add_expense)
+        self.btn_delete.clicked.connect(self.delete_expense)
 
         self.setup_layout()
         self.show()
@@ -60,3 +65,45 @@ class ExpenseApp(QWidget):
     def populate_dropdown(self):
         categories = ["Food", "Rent", "Bills", "Entertainment", "Shopping", "Other"]
         self.dropdown.addItems(categories)
+    
+    def load_table_data(self):
+        expenses = fetch_expenses()
+        self.table.setRowCount(0)
+        for row_idx, expense in enumerate(expenses):
+            self.table.insertRow(row_idx)
+            for col_idx, data in enumerate(expense):
+                self.table.setItem(row_idx, col_idx, QTableWidgetItem(str(data)))
+
+    def clear_inputs(self):
+        self.date_box.setDate(QDate.currentDate())
+        self.dropdown.setCurrentIndex(0)
+        self.amount.clear()
+        self.description.clear()
+
+    def add_expense(self):
+        date = self.date_box.date().toString("yyyy-MM-dd")
+        category = self.dropdown.currentText()
+        amount = self.amount.text()
+        description = self.description.text()
+
+        if not amount or not description:
+            QMessageBox.warning(self, "Input Error", "Amount and Description can't be empty")
+            return
+        
+        if add_expenses(date, category, amount, description):
+            self.load_table_data()
+            self.clear_inputs()
+        else:
+            QMessageBox.warning(self, "Error", "Failed to add expense")
+
+    def delete_expense(self):
+        selected_row = self.table.currentRow()
+        if selected_row == -1:
+            QMessageBox.warning(self, "Uh oh", "You need to choose a row to delete")
+            return
+        
+        expense_id = int(self.table.item(selected_row, 0).text())
+        confirm = QMessageBox.question(self, "Confirm", "Are you sure you want to delete?", QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+
+        if confirm == QMessageBox.StandardButton.Yes and delete_expenses(expense_id):
+            self.load_table_data()
